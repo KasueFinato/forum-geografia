1be91eb48b8ac8a8a9587de6beae5ca9d8b2daf4
Julie Cruz <juliebcruzz@gmail.com>
2014-11-11 18:23:34 -0200

    validação inicial de assunto #46

--- forum-geografia/app/models/assunto.rb
+++ forum-geografia/app/models/assunto.rb
@@ -1,3 +1,9 @@
 class Assunto < ActiveRecord::Base
 	has_many :posts
+
+	validates :assunto,
+  			#format: { with: /\A[a-zA-Z]+\z/ }, ????
+  			length: { in: 10..30 },
+  			presence: {message: 'O assunto deve ser preenchido'},
+  			uniqueness: {message: 'Esse assunto já foi cadastrado'}
 end
