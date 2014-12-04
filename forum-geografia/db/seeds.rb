2300fcee4078ed08b695c92c117334fcad70438d
kasuefinato <kasuefinato@gmail.com>
2014-12-04 12:13:22 -0200

    itens criados

--- forum-geografia/db/seeds.rb
+++ forum-geografia/db/seeds.rb
@@ -21,3 +21,13 @@
 				  
 
 	@usuario = Usuario.new nome: 'Karoline Miki', email: 'kasuefinato@gmail.com', login: 'kasuemiki', senha: 'chinaliexpres'
+
+	Assunto.create assunto: 'Crise hidrica em sp', tipo: 1
+
+	Assunto.create assunto: 'Exodo rural nos anos 30', tipo: 2
+
+	Assunto.create assunto: 'A consistencia dos BRICS', tipo: 2
+
+	Post.create conteudo: 'Liberdade eh o bairro que mais gasta agua, voces sabem pq?' , assunto_id: 1
+
+	Post.create conteudo: 'Quais as conseguencias da industrializaçao dos anos 30?' , assunto_id: 2
